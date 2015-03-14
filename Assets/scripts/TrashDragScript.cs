8a24392a8221650fbbc4adf8e70f7943dd2e35ce
Frances Russell <franceskrussell@gmail.com>
2015-03-14 10:52:47 -0700

    Added trash drag hack script

---
+++ Assets/scripts/TrashDragScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashDragScript : MonoBehaviour {
+
+	Vector3 screenSpace;
+	Vector3 offset;
+	void OnMouseDown()
+	{	
+		//translate the cubes position from the world to Screen Point
+		screenSpace = Camera.main.WorldToScreenPoint(transform.position);
+		//calculate any difference between the cubes world position and the mouses Screen position converted to a world point  
+		offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, screenSpace.z));
+
+		// Stop the trash from moving to the right
+	}
+	
+	void OnMouseDrag()
+	{
+		Vector3 curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z); 
+		Vector3 curPosition =  Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
+		transform.position = curPosition;
+	}
+}
