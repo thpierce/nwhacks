caff0ba297067c4739c993a0060df78935c6eef6
thpierce <thomas.pierce92@gmail.com>
2015-03-14 16:16:14 -0700

    Hacked collisions, waste types

---
+++ Assets/scripts/WasteCollisionScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WasteCollisionScript : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "WasteItem"){
+			Physics2D.IgnoreCollision(coll.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+		}
+	}
+}
---
+++ Assets/scripts/WasteTypeScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WasteTypeScript : MonoBehaviour {
+
+	public string type;
+	private string[] types = new string[3] {"compost", "container", "paper"};
+
+	// Use this for initialization
+	void Start () {
+
+		int randType = Random.Range (0, 3);
+		string[] images = null;
+
+		type = types[randType];
+		switch (randType){
+			case 0:
+			images =  new string[3] {"banana", "apple", "fish"};
+			break;
+			case 1:
+			images =  new string[4] {"bottle", "milk", "tuna_can", "can"};
+			break;
+			case 2:
+			images =  new string[3] {"newspaper", "box", "pizza_box"};
+			break;
+		}
+
+		string randImage = images[Random.Range(0, images.Length)];
+		gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load(randImage, typeof(Sprite)) as Sprite;
+	}
+}
