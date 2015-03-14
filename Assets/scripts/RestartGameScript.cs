1002c68137007abb8b46679cb7837ea153bbeba0
thpierce <thomas.pierce92@gmail.com>
2015-03-14 00:29:55 -0700

    Hacked the re/start buttons

---
+++ Assets/scripts/RestartGameScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartGameScript : MonoBehaviour {
+
+	void OnGUI () {
+		const int width = 100;
+		const int height = 50;
+
+		Rect start = new Rect(
+			Screen.width/2 - width/2,
+			3*Screen.height/4 - height/2,
+			width,
+			height);
+
+		if (GUI.Button (start, "Restart Game")){
+			Application.LoadLevel("menu");
+		}
+	}
+}
---
+++ Assets/scripts/StartGameScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameScript : MonoBehaviour {
+
+	void OnGUI () {
+		const int width = 100;
+		const int height = 50;
+
+		Rect start = new Rect(
+			Screen.width/2 - width/2,
+			3*Screen.height/4 - height/2,
+			width,
+			height);
+
+		if (GUI.Button (start, "Start Game")){
+			Application.LoadLevel("stage1");
+		}
+	}
+}
