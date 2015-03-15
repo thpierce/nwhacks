9cfb5712a5e7beb947f28bd711fb22c8eea58e11
Frances Russell <franceskrussell@gmail.com>
2015-03-14 17:25:19 -0700

    Added more graphics and hacked bins

--- Assets/scripts/LifeScript.cs
+++ Assets/scripts/LifeScript.cs
@@ -5,7 +5,6 @@ public class LifeScript : MonoBehaviour {
 	private int livesRemaining = 3;
 
 	public void loseLife(){
-		Destroy (transform.Find("/Level/2 - Foreground/life" + livesRemaining.ToString()).gameObject);
 		livesRemaining -= 1;
 		if (livesRemaining == 0){
 			Application.LoadLevel("gameover");
