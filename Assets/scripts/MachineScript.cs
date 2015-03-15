0f89a678471725f922c74cc0fe430d976a7dd2b7
thpierce <thomas.pierce92@gmail.com>
2015-03-14 17:26:27 -0700

    Hacked machineScript

--- Assets/scripts/MachineScript.cs
+++ Assets/scripts/MachineScript.cs
@@ -2,24 +2,10 @@
 using System.Collections;
 
 public class MachineScript : MonoBehaviour {
-	//--------------------------------
-	// 1 - Designer variables
-	//--------------------------------
-	
-	/// <summary>
-	/// Projectile prefab for shooting
-	/// </summary>
-
 	public Transform garbagePrefab;
-	/// <summary>
-	/// Cooldown in seconds between two shots
-	/// </summary>
 	public double startTime;
 	public double nextTime;
 	public Vector3 randomPosition;
-	//--------------------------------
-	// 2 - Cooldown
-	//--------------------------------
 
 	
 	void Start()
@@ -33,28 +19,16 @@ public class MachineScript : MonoBehaviour {
 		if (Time.time > nextTime){
 			double minutes = (Time.time - startTime)/5.0;
 			nextTime = Time.time + 5.001 / (minutes + 1.0);
-			MakeNewGarbage (true);
+			MakeNewGarbage ();
 		}
 	}
 	
-	//--------------------------------
-	// 3 - Shooting from another script
-	//--------------------------------
-	
-	/// <summary>
-	/// Create a new projectile if possible
-	/// </summary>
-	public void MakeNewGarbage(bool isEnemy)
+	public void MakeNewGarbage()
 	{
-			int randomY = Random.Range (-3, 3);
-			randomPosition = new Vector3 (-7,randomY,5);
-			var garbageTransform = Instantiate(garbagePrefab) as Transform;
-			garbageTransform.position = randomPosition;
-			//TrashMoveScript move = garbageTransform.gameObject.GetComponent<TrashMoveScript>();
-//			if (move != null)
-//			{
-//				move.direction = this.transform.right; // towards in 2D space is the right of the sprite
-//			}
+		int randomY = Random.Range (-3, 3);
+		randomPosition = new Vector3 (transform.position.x,randomY,5);
+		var garbageTransform = Instantiate(garbagePrefab) as Transform;
+		garbageTransform.position = randomPosition;
 	}
 
 }
---
+++ Assets/scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	private int score;
+
+	void Start () {
+		score = 0;
+		drawScore();
+	}
+	
+	public void UpdateScore (int change) {
+		score += change;
+		drawScore();
+	}
+
+	void drawScore() {
+		GetComponent<GUIText>().text = "Score: " + score.ToString();
+	}
+}
